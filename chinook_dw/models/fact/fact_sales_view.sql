43080e2854de0d87290d73397755d91c2e6f819b
jdaguilar <jorgeaguilar2761@gmail.com>
2023-07-27 18:03:59 -0500

    load view fact sales

--- chinook_dw/models/fact/fact_sales_view.sql
+++ chinook_dw/models/fact/fact_sales_view.sql
@@ -1,4 +1,10 @@
 
+{{
+    config({
+        "materialized": "view",
+    })
+}}
+
 with stg_invoice as (
     select *
     FROM {{ ref('stg_invoice') }}
@@ -26,10 +32,10 @@ select
 	si.invoice_date,
 	sil.invoice_id,
     sil.invoice_line_id,
-	sil.track_id,
-	si.customer_id,
-    dc.employee_id,
-	dl.location_id,
+	dt.track_name,
+	dc.first_name as customer_first_name,
+	dl.city as billing_city,
+    de.first_name as employee_first_name,
 	sil.quantity,
 	sil.unit_price
 from stg_invoice si
@@ -43,5 +49,4 @@ LEFT JOIN dim_track dt
     ON sil.track_id = dt.track_id
 LEFT JOIN dim_employee de
     ON dc.employee_id = de.employee_id
-
 order by invoice_date desc
