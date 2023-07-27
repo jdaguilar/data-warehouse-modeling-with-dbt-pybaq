5a313f24a1e777d5dd7857739e0f29026ab47346
jdaguilar <jorgeaguilar2761@gmail.com>
2023-07-27 11:21:15 -0500

    update dim employee

--- chinook_dw/models/dim/dim_employee.sql
+++ chinook_dw/models/dim/dim_employee.sql
@@ -1,19 +1,11 @@
-with stg_employee as (
-	SELECT * FROM {{ ref('stg_employee') }}
-),
 
-dim_employee as (
-	select
-		employee_id,
-		last_name,
-		first_name,
-		title,
-		birth_date,
-		hire_date,
-		city,
-		state,
-		country
-	from stg_employee
-)
-select *
-from dim_employee
+{% set columns_to_exclude = ['address', 'reports_to', 'state', 'postal_code', 'phone', 'fax', 'email'] %}
+
+select
+{{ dbt_utils.star(
+    from=ref('stg_employee'), 
+    except=columns_to_exclude, 
+    ) 
+}}
+from {{ ref('stg_employee') }}
+
